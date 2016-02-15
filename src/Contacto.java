c86515e3ceecc02736bb5146d9332c66a5e32058
Fercho <gomez.alejandre@gmail.com>
2016-02-15 17:56:30 -0600

    Agregar DiagramaUML

---
+++ src/Contacto.java
@@ -0,0 +1,56 @@
+public class Contacto {
+
+    private String nombre;
+
+    private String apellido;
+
+    private ArrayList telefono;
+
+    private ArrayList redSocial;
+
+    private String web;
+
+    private Image foto;
+
+    public String getNombre() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public String getApellido() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public ArrayList getTelefono() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public ArrayList getRedSocial() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public String getWeb() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Image getFoto() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void setNombre(String nombre) {
+    }
+
+    public void setApellido(String apellido) {
+    }
+
+    public void setTelefono(ArrayList telefono) {
+    }
+
+    public void setRedSocial(ArrayList redSocial) {
+    }
+
+    public void setWeb(String web) {
+    }
+
+    public void setFoto(Image foto) {
+    }
+}
